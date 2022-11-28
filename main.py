c3cc854eaee8176dba56f514bbbff5f65f303818
katesorotos <44615640+katesorotos@users.noreply.github.com>
2022-11-28 21:19:01 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,25 +2,26 @@ import requests
 from dotenv import load_dotenv
 import os
 
-load_dotenv() # take environment variables from .env.
+load_dotenv()  # take environment variables from .env.
+
 
 def send_email_reply(ticket_id, user, email_text, authentication):
 
-    """Function to automatically send email reply to Gorgias ticket
+    """Function to automatically send email reply and internal note to ticket using Gorgias REST API
         Parameters:
         Ticket ID(int),
         Agent User(int),
         Email Text(string),
-        Authentication"""
-
+        Authentication,
+        """
     url = f'https://dgexercise.gorgias.com/api/tickets/{ticket_id}'
+    # formatted string to include expression {ticket_id}
     headers = {
         "accept": "application/json",
         "content-type": "application/json",
         "authorization": None
     }
-    headers.update({'authorization' : authentication})
-
+    headers.update({'authorization': authentication})  # define authentication credentials
     payload = {
         "assignee_team": None,
         "assignee_user": None,
@@ -45,13 +46,47 @@ def send_email_reply(ticket_id, user, email_text, authentication):
         "trashed_datetime": None,
         "updated_datetime": "2020-01-27T10:42:21.932637",
         "via": "email"
-    }
+    }  # information sent when making api request
     payload.update({'messages': email_text})
     payload["customer"]["id"] = user
 
+    #   if internal_note:
+    #       payload['source']['type'] = None
+    #       internal_note_dict = {'type': 'internal-note'}
+    #       payload['source'].update(internal_note_dict)
+
     response = requests.put(url, json=payload, headers=headers)
+    # in-built method put() for making a PUT request to URI specified
 
     print(response.text)
 
-send_email_reply(13228870, 36331423, "test",os.environ.get("AUTHORISATION"))
 
+send_email_reply(13228870, 36331423, "test", os.environ.get("AUTHORISATION")) # call function, specifying value for arguments
+
+
+def leave_internal_note(ticket_id, user, internal_note, authentication):
+
+    payload = {
+        "sender": {"id": None},
+        "source": {"type": "internal-note"},
+        "body_text": "This is an internal note test",
+        "channel": "internal-note",
+        "via": "api"
+    }
+    payload["sender"]["id"] = user
+    payload.update({'body_text': internal_note})
+    url = f'https://dgexercise.gorgias.com/api/tickets/{ticket_id}/messages'
+
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "authorization": None
+    }
+    headers.update({'authorization': authentication})
+
+    response = requests.post(url, json=payload, headers=headers)
+
+    print(response.text)
+
+
+leave_internal_note(13228870, 36331423, "This is an internal note test", os.environ.get("AUTHORISATION"))
